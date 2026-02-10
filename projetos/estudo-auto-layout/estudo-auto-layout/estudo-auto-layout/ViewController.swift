23d04d549e9235dd4b191fa0571ff9d3af5be50f
Felipe Barreto <felipebarreto1050@gmail.com>
2026-02-10 15:06:17 -0300

    no message

---
+++ projetos/estudo-auto-layout/estudo-auto-layout/estudo-auto-layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  estudo-auto-layout
+//
+//  Created by Felipe iOS on 10/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
